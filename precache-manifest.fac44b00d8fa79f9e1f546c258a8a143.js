a8f2f843756440e1eef373de75dddc6d9e514d61
lakshy pachory <luckyron1279@gmail.com>
2020-05-18 16:52:45 +0530

    Updates

---
+++ precache-manifest.fac44b00d8fa79f9e1f546c258a8a143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bc308dd4f1a75fd925044defe47f30",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "fbeb98b8a45ea028187a",
+    "url": "/first-react-app/static/css/main.af24ad32.chunk.css"
+  },
+  {
+    "revision": "9755a93378621ba3be30",
+    "url": "/first-react-app/static/js/2.6a12f6c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.6a12f6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbeb98b8a45ea028187a",
+    "url": "/first-react-app/static/js/main.7cdf6d82.chunk.js"
+  },
+  {
+    "revision": "e919864d395554a94ce1",
+    "url": "/first-react-app/static/js/runtime-main.240efff0.js"
+  }
+]);
